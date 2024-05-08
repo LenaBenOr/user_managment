8af07ac50476a95964de8fe83f2a13932b58c74f
Lena Ben Or <lena.benor@gmail.com>
2024-05-08 10:25:06 +0300

    input user_name

--- users_managment.py
+++ users_managment.py
@@ -9,20 +9,28 @@ class User:
         self.password = password
 
 def get_user_name():
+    
+    user_name = input("please eneter your name: ")
+    if user_name is None: 
+        user_name = input("please eneter your name: ")
+    print(user_name)
 
-def get_user_password(): 
 
 
-def is_admin(name, password): 
+#def get_user_password(): 
 
-def create_user():
-def update_user():
-def delete_user():
 
-def connect_to_db():
-def create_db(): 
+#def is_admin(name, password): 
+
+#def create_user():
+#def update_user():
+#def delete_user():
+
+#def connect_to_db():
+#def create_db(): 
 
 def main(): 
+    get_user_name()
     
 
 
