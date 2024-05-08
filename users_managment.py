47b05315811a07e1b8aa78fef47dd4babec800f8
Lena Ben Or <lena.benor@gmail.com>
2024-05-08 09:55:22 +0300

    initial commit

---
+++ users_managment.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+class User: 
+    def __init__(self, user_id, user_name, first_name, last_name, password):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+def get_user_name():
+
+def get_user_password(): 
+
+
+def is_admin(name, password): 
+
+def create_user():
+def update_user():
+def delete_user():
+
+def connect_to_db():
+def create_db(): 
+
+def main(): 
+    
+
+
+if __name__ == "__main__":
+    main()
